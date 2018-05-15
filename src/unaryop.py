55b78e0d22da8a5459a0a47d126cd3d297126d58
Gustavo Efeiche <gustavo.efeiche@gmail.com>
2018-05-14 21:38:19 -0300

    Add type checking for unary operations

--- src/unaryop.py
+++ src/unaryop.py
@@ -1,15 +1,25 @@
 from node import Node
 
+
 class UnaryOp(Node):
     def eval(self, st):
         if self.children:
             result = self.children[0].eval(st)
-        
+
         if self.value == "PLUS":
-            return result
-        
+            if not isinstance(result, bool):
+                return result
+
+            raise ValueError("Incompatible types: got \"Boolean\" expected \"Integer\"")
+
         elif self.value == "MINUS":
-            return -result
+            if not isinstance(result, bool):
+                return -result
+
+            raise ValueError("Incompatible types: got \"Boolean\" expected \"Integer\"")
 
         elif self.value == "NOT":
-        	return not result
+            if isinstance(result, bool):
+                return not result
+            else:
+                return ~result
