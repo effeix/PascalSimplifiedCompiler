3eb5005f0bfd8b30e9b1d151d34a9ecd5eec8011
Gustavo Efeiche <gustavo.efeiche@gmail.com>
2018-05-18 23:38:11 -0300

    Convert indentation: tabs -> spaces

--- src/vardecnode.py
+++ src/vardecnode.py
@@ -1,6 +1,7 @@
 from node import Node
 
+
 class VarDecNode(Node):
-	def eval(self, st):
-		for child in self.children:
-			child.eval(st)
+    def eval(self, st):
+        for child in self.children:
+            child.eval(st)
