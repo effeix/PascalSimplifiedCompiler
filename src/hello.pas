8d45be88c3a736b289cc93ff6ed789c76b99a2ff
Gustavo Efeiche <gustavo.efeiche@gmail.com>
2018-05-18 22:28:46 -0300

    Test file with all features

--- src/hello.pas
+++ src/hello.pas
@@ -1,34 +1,58 @@
-PROGRAM a;
+PROGRAM test_all;
 
 VAR
-    x, y, z: integer;
-    test: boolean;
+    a, b, c, integer_return: integer;
+    boolean_return: boolean;
 
-FUNCTION test_a(): integer;
-BEGIN
-    write(1);
-END;
+FUNCTION function_integer_return(): integer;
+    VAR
+        a, b: integer;
 
-FUNCTION test_b(): integer;
-BEGIN
-    write(2);
-END;
-
-BEGIN
-    x := 0;
-    y:= 10;
-
-    write(1 AND 2);
-
-    WHILE (y > 0) DO
     BEGIN
-        x := x + 2;
-        write(x);
-        y := y - 1;
+        a := 1;
+        b := 2;
+        function_integer_return := a + b;
     END;
 
-    IF (x > 1) THEN
+FUNCTION function_boolean_return(): boolean;
+    VAR
+        a, b: boolean;
+
     BEGIN
-        write(not x);
+        a := true;
+        b := false;
+        function_boolean_return := a and b;
+    END;
+
+BEGIN
+
+    c := read();
+    write(c);
+
+    write(1 and 2);
+    write(1 and 1);
+    write(not -3);
+    write(function_integer_return());
+
+    write(true and true);
+    write(true or false);
+    write(not true);
+    write(function_boolean_return());
+
+    a := 0;
+    b := 10;
+
+    WHILE (b > 0) DO
+    BEGIN
+        IF (b > 5) THEN
+            BEGIN
+                write(123456789);
+            END
+        ELSE
+            BEGIN
+                write(987654321);
+            END;
+
+        b := b - 1;
     END;
 END.
