4dbe627936a19a251a79820b3d5702a0653b8cc7
Gustavo Efeiche <gustavo.efeiche@gmail.com>
2018-05-21 16:09:01 -0300

    Generate assembly for global variables

--- src/vardecnode.py
+++ src/vardecnode.py
@@ -1,7 +1,13 @@
 from node import Node
+from assembly import Assembly
 
 
 class VarDecNode(Node):
     def eval(self, st):
         for child in self.children:
+            if st.is_global:
+                self.generate_assembly(child.value, st.identifier)
             child.eval(st)
+
+    def generate_assembly(self, var_name, st_id):
+        Assembly.append(f"""  {var_name}_{st_id} RESD 1""")
