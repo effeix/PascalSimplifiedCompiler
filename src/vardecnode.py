5afc2882fe36e1623ebd225581e10922516ff4d7
Gustavo Efeiche <gustavo.efeiche@gmail.com>
2018-05-21 16:56:23 -0300

    Make generate_assembly method private

--- src/vardecnode.py
+++ src/vardecnode.py
@@ -6,11 +6,11 @@ class VarDecNode(Node):
     def eval(self, st):
         for child in self.children:
             if st.is_global:
-                self.generate_assembly(child.value, st.identifier)
+                self.__generate_assembly(child.value, st.identifier)
             child.eval(st)
 
         if st.is_global:
             Assembly.append("""  res RESB 1""")
 
-    def generate_assembly(self, var_name, st_id):
+    def __generate_assembly(self, var_name, st_id):
         Assembly.append(f"""  {var_name}_{st_id} RESD 1""")
