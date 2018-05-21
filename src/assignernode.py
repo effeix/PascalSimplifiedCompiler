f0b27560d26ad74b59607c1813f72e580d67a243
Gustavo Efeiche <gustavo.efeiche@gmail.com>
2018-05-21 16:55:51 -0300

    Generate assembly for variable assignment

--- src/assignernode.py
+++ src/assignernode.py
@@ -1,4 +1,6 @@
 from node import Node
+from assembly import Assembly
+
 
 class AssignerNode(Node):
     def __init__(self, _vartype=None, _value=None):
@@ -6,9 +8,14 @@ class AssignerNode(Node):
         self.children = []
         self.vartype = _vartype
 
-    def eval(self, st):        
+    def eval(self, st):
         if self.vartype:
             st.create_identifier(self.value, self.vartype)
         else:
             val = self.children[0].eval(st)
             st.set_identifier(self.value, val)
+            if st.is_global:
+                self.__generate_assembly(self.value, st.identifier)
+
+    def __generate_assembly(self, value, st_id):
+        Assembly.append(f"""  MOV [{value}_{st_id}], EBX""")
