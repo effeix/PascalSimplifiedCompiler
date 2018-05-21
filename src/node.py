557e6409802bcf8a11a3ec6d70ef1cdbe1a2ae05
Gustavo Efeiche <gustavo.efeiche@gmail.com>
2018-05-21 10:37:23 -0300

    Add unique identifier generation to semantic analysis

--- src/node.py
+++ src/node.py
@@ -1,7 +1,11 @@
+from identifier import Identifier
+
+
 class Node():
     def __init__(self, _value=None):
         self.value = _value
         self.children = []
-    
+        self.identifier = Identifier.get_new()
+
     def set_child(self, child):
         self.children.append(child)
