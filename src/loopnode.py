90e403d1d65168b01e006198836953f64f74cc76
Gustavo Efeiche <gustavo.efeiche@gmail.com>
2018-05-29 17:08:40 -0300

    Generate non-optimized assembly for loops (no support for nested loops)

--- src/loopnode.py
+++ src/loopnode.py
@@ -1,6 +1,28 @@
 from node import Node
+from assembly import Assembly
+
 
 class LoopNode(Node):
     def eval(self, st):
-        while self.children[0].eval(st) == True:
-            self.children[1].eval(st)
+
+        condition = self.children[0]
+        statements = self.children[1]
+
+        self.__generate_assembly("LOOP_ENTER")
+
+        while condition.eval(st, n_id=self.identifier):
+            statements.eval(st)
+
+        self.__generate_assembly("LOOP_EXIT")
+
+    def __generate_assembly(self, instruction):
+        if instruction == "LOOP_ENTER":
+            commands = f"""  WHILE_{self.identifier}:"""
+
+        elif instruction == "LOOP_EXIT":
+            commands = [
+                f"""  JMP WHILE_{self.identifier}""",
+                f"""  EXIT_{self.identifier}:"""
+            ]
+
+        Assembly.append(commands)
